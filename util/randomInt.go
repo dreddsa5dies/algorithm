85a53831e17ce78904a9ee19de95fea1cbad8dee
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2016-07-19 13:42:00 +0300

    update randomInt

--- util/randomInt.go
+++ util/randomInt.go
@@ -5,7 +5,7 @@ import "math/rand"
 // RandomInt create random array []int
 func RandomInt() []int {
 	list := rand.Perm(20)
-	for i := range list {
+	for i := 0; i < len(list); i++ {
 		j := rand.Intn(i + 1)
 		list[i], list[j] = list[j], list[i]
 	}
